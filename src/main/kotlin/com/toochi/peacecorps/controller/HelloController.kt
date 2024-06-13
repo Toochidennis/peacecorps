c07928e7f4a144cab804cfccc9cd021cd50916f7
Toochidennis <dennistoochukwu@gmail.com>
2024-06-13 13:12:18 +0100

    New commit

---
+++ src/main/kotlin/com/toochi/peacecorps/controller/HelloController.kt
@@ -0,0 +1,14 @@
+package com.toochi.peacecorps.controller
+
+import javafx.fxml.FXML
+import javafx.scene.control.Label
+
+class HelloController {
+    @FXML
+    private lateinit var welcomeText: Label
+
+    @FXML
+    private fun onHelloButtonClick() {
+        welcomeText.text = "Welcome to JavaFX Application!"
+    }
+}
