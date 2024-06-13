4d2f2194a88113255c280f9c443a0b9ab2b91cf5
Toochidennis <dennistoochukwu@gmail.com>
2024-06-13 13:12:36 +0100

    New commit

---
+++ src/main/kotlin/com/toochi/peacecorps/Launcher.kt
@@ -0,0 +1,9 @@
+package com.toochi.peacecorps
+
+import com.toochi.peacecorps.splashscreen.HelloApplication
+import javafx.application.Application
+
+
+fun main() {
+    Application.launch(HelloApplication::class.java)
+}
